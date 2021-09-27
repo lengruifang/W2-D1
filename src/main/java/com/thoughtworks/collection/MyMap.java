8e524ad9e89bbc6eec355e2e68da7638920fe105
陈俊生 <2810955497@qq.com>
2021-09-27 10:50:37 +0800

    用ASCII码成功通过测试，用第二个方法时遇到一点问题。

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -11,17 +11,26 @@ import java.util.stream.Collectors;
 public class MyMap {
 
     List<Integer> array;
-    private String resultString (Integer elemet){
-        if (elemet<=26) {
-            int ASCIICode = elemet + 96;
-            return String.valueOf((char) ASCIICode);
-        }else {
-            int ASCIICode1= (int) Math.floor(elemet/26)+96;
-            int ASCIICode2=elemet%26+96;
-            String string1=String.valueOf((char) ASCIICode1);
-            String string2=String.valueOf((char) ASCIICode2);
-            return string1.concat(string2);
-        }
+    private String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    private List<String> letterList = Arrays.asList(letters);
+    private String resultString (Integer element){
+           if (element <= 26) {
+                int ASCIICode = element + 96;
+                return String.valueOf((char) ASCIICode);
+            } else if (element%26!=0){
+                int ASCIICode1 = (int) Math.floor(element / 26) + 96;
+                int ASCIICode2 = element % 26 + 96;
+                String string1 = String.valueOf((char) ASCIICode1);
+                String string2 = String.valueOf((char) ASCIICode2);
+                return string1.concat(string2);
+            }else {
+               int ASCIICode1 = (int) Math.floor(element / 26) + 96-1;
+               int ASCIICode2 = 26+96;/*当对应字符应该是"az","bz"等等时*/
+               String string1 = String.valueOf((char) ASCIICode1);
+               String string2 = String.valueOf((char) ASCIICode2);
+               return string1.concat(string2);
+           }
     }
 
     public MyMap(List<Integer> array) {
@@ -34,18 +43,20 @@ public class MyMap {
 
     public List<String> mapLetter() {
         List<String> resultStrings=new ArrayList<>();
-        for (Integer elemet : array){
-            resultStrings.add(resultString(elemet));
+        for (Integer element : array){
+            resultStrings.add(resultString(element));
         }
-        return  resultStrings;
+        /*return  resultStrings;*/
+        return array.stream().map(elem->letters[elem-1]).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
         List<String> resultStrings=new ArrayList<>();
-        for (Integer elemet : array){
-            resultStrings.add(resultString(elemet));
+        for (Integer element : array){
+            resultStrings.add(resultString(element));
         }
         return  resultStrings;
+        /*return array.stream().map(elem->).collect(Collectors.toList());*/
     }
 
     public List<Integer> sortFromBig() {
